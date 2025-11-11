867d7337e03134352e31053f0706cf07b4e0d8eb
simpleDevWeb3 <g64bit507@gmail.com>
2025-11-11 14:26:50 +0800

    adding message

--- KopitiamApi/Controllers/WeatherForecastController.cs
+++ KopitiamApi/Controllers/WeatherForecastController.cs
@@ -21,8 +21,10 @@ namespace KopitiamApi.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            Console.WriteLine($"Generating weather forecast for day");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+       
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
